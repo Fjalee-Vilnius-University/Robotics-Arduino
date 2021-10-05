f1690c14802e4b62d56c9b5bff2e6cf712809332
Rytis Cepulis <cepRytis@gmail.com>
2021-10-05 16:01:13 +0300

    make blue light with analog

--- mini-examples-arduino/LED-switch-to-voltage/LED-switch-to-voltage.ino
+++ mini-examples-arduino/LED-switch-to-voltage/LED-switch-to-voltage.ino
@@ -17,35 +17,25 @@ void setup() {
 void loop() {
   int pValue = analogRead(POTENTIOMETER);
 
-  int stThird = 1023 / 3;
-  int ndThird = 1023 / 3 * 2;
-  int rdThird = 1023;
+  int potentiometerRangeTo = 1023;
+  int potentiometerRangeHalf = potentiometerRangeTo / 2;
 
-  if(pValue >= 0 && pValue <= stThird){
+  analogWrite(BLUE_LED, pValue/4);
+
+  if(pValue >= 0 && pValue <= potentiometerRangeHalf){
     onOnlyGreenLed();
   }
-  else if(pValue > stThird && pValue <= ndThird){
-    onOnlyBlueLed();
-  }
-  else if(pValue > ndThird && pValue <= rdThird){
+  else if(pValue > potentiometerRangeHalf && pValue <= potentiometerRangeTo){
     onOnlyRedLed();
   }
 }
 
 void onOnlyGreenLed(){
   digitalWrite(GREEN_LED, HIGH);
-  digitalWrite(BLUE_LED, LOW);
-  digitalWrite(RED_LED, LOW);
-}
-
-void onOnlyBlueLed(){
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, HIGH);
   digitalWrite(RED_LED, LOW);
 }
 
 void onOnlyRedLed(){
   digitalWrite(GREEN_LED, LOW);
-  digitalWrite(BLUE_LED, LOW);
   digitalWrite(RED_LED, HIGH);
 }
