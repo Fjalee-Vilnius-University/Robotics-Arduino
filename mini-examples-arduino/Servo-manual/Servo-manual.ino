86830afddc2a96f7621ee10f6eaa06f8890ad466
Rytis Cepulis <cepRytis@gmail.com>
2021-10-26 15:47:18 +0300

    trying to create funciton which will control manually

--- mini-examples-arduino/Servo-manual/Servo-manual.ino
+++ mini-examples-arduino/Servo-manual/Servo-manual.ino
@@ -6,21 +6,28 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 100; i++)
-  {
-    digitalWrite(SERVO, 1);
-    delay(1);
-    digitalWrite(SERVO, 0);
-    delay(19);
-    Serial.write("1");
+  servoRotate(90);
+  delay(1000);
+  servoRotate(180);
+  delay(1000);
+}
+
+void servoRotate(int deg){
+  if (deg < 0 || deg >180){
+    return;
   }
+
+  int totalImpulse = 20000;
+  int mcsForOneDeg = totalImpulse / 180;
+  int mcsOn = deg * mcsForOneDeg;
+  int mcsOff = totalImpulse - mcsOn;
 
   for (int i = 0; i < 100; i++)
   {
     digitalWrite(SERVO, 1);
-    delay(2);
+    delayMicroseconds(mcsOn);
     digitalWrite(SERVO, 0);
-    delay(18);
-    Serial.write("0");
+    delayMicroseconds(mcsOff);
+    Serial.write("1");
   }
 }
