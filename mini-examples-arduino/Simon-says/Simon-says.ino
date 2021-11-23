29ad7009b3fe74644bf77866b6751abc7265c343
Rytis Cepulis <cepRytis@gmail.com>
2021-11-23 15:36:17 +0200

    button map to leds

--- mini-examples-arduino/Simon-says/Simon-says.ino
+++ mini-examples-arduino/Simon-says/Simon-says.ino
@@ -1,37 +1,81 @@
 #define SPEAKER 7
-#define RED 5
-#define GREEN 13
-#define BLUE 3
-#define YELLOW 10
+
+#define RED_LED 5
+#define GREEN_LED 13
+#define BLUE_LED 3
+#define YELLOW_LED 10
+
+#define RED_BUTTON 2
+#define GREEN_BUTTON 6
+#define BLUE_BUTTON 9
+#define YELLOW_BUTTON 12
 
 int delayInMs = 50;
 
 void setup() {
   pinMode(SPEAKER, OUTPUT);
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  pinMode(BLUE, OUTPUT);
-  pinMode(YELLOW, OUTPUT);
+
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+
+  pinMode(RED_BUTTON, INPUT_PULLUP);
+  pinMode(GREEN_BUTTON, INPUT_PULLUP);
+  pinMode(BLUE_BUTTON, INPUT_PULLUP);
+  pinMode(YELLOW_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
-  int redLed = RED;
-  int greenLed = GREEN;
-  int blueLed = BLUE;
-  int yellowLed = YELLOW;
+  int redLed = RED_LED;
+  int greenLed = GREEN_LED;
+  int blueLed = BLUE_LED;
+  int yellowLed = YELLOW_LED;
 
-  blinkLed(redLed);
-  blinkLed(greenLed);
-  blinkLed(blueLed);
-  blinkLed(yellowLed);
+  int redButton = RED_BUTTON;
+  int greenButton = GREEN_BUTTON;
+  int blueButton = BLUE_BUTTON;
+  int yellowButton = YELLOW_BUTTON;
+
+  buttonAction(redLed, redButton);
+  buttonAction(greenLed, greenButton);
+  buttonAction(blueLed, blueButton);
+  buttonAction(yellowLed, yellowButton);
 }
 
 void blinkLed(int led){
-  if (led != RED && led != GREEN && led != BLUE && led != YELLOW){
+  if (!isLed(led)){
     return;
   }
 
   digitalWrite(led, HIGH);
   delay(delayInMs);
   digitalWrite(led, LOW);
+}
+
+bool buttonAction(int led, int button){
+  if (!isButton(button) || !isLed(led)){
+    return false;
+  }
+
+  int buttonState = digitalRead(button);
+
+  if(buttonState == LOW){
+    blinkLed(led);
+    return true;
+  }
+}
+
+bool isLed(int led){
+  if (led != RED_LED && led != GREEN_LED && led != BLUE_LED && led != YELLOW_LED){
+    return false;
+  }
+  return true;
+}
+
+bool isButton(int button){
+  if (button != RED_BUTTON && button != GREEN_BUTTON && button != BLUE_BUTTON && button != YELLOW_BUTTON){
+    return false;
+  }
+  return true;
 }
